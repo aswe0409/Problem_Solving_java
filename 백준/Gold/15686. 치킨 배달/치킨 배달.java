7c935bfca3ca5c8ab0bda24bf29c6d5e72376b83
정석영 <aswe0409@naver.com>
2024-04-09 23:13:36 +0900

    [Gold V] Title: 치킨 배달, Time: 172 ms, Memory: 13956 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -13,65 +13,60 @@ public class Main {
 			this.col = col;
 		}
 	}
-	
-	
-	static int n,m, min;
-	static int [][] arr;
+	static int ans = 0;
+	static int n,m,min;
 	static List<Pos> home = new ArrayList<>();
-	static List<Pos> kfc = new ArrayList<>();
+	static List<Pos> chicken = new ArrayList<>();
 	static boolean visit[];
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			n = Integer.parseInt(st.nextToken());
-			m = Integer.parseInt(st.nextToken()); // 최대 치킨 집 수
-			arr = new int[n][n];
-			
-			for(int i = 0; i < n; i++) {
-				StringTokenizer str = new StringTokenizer(br.readLine());
-				for(int j = 0; j < n; j++) {
-					int temp = Integer.parseInt(str.nextToken());
-					if(temp == 1) { // 집 일 경우
-						home.add(new Pos(i,j));
-					}
-					else if(temp == 2) { //치킨집일 경우
-						kfc.add(new Pos(i,j));
-					}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer str = new StringTokenizer(br.readLine());
+			for(int j = 0; j < n; j++) {
+				int temp = Integer.parseInt(str.nextToken());
+				if(temp == 1) { // 집 일 경우
+					home.add(new Pos(i,j));
 				}
-			} // i for
-			
-			min = Integer.MAX_VALUE;
-			visit = new boolean[kfc.size()];
-			func(0,0);
-			System.out.println(min);
-		}
-	}
-	
-	private static void func(int cnt, int start) {
-		if(cnt == m) {
-			int dist = 0;
-			
-			for(int i = 0; i < home.size(); i++) {
-				int temp_min = Integer.MAX_VALUE;
-				for(int j = 0; j < kfc.size(); j++) {
-					if(visit[j]) {
-						int temp_dist = Math.abs(home.get(i).row -kfc.get(j).row);
-						temp_dist +=  Math.abs(home.get(i).col -kfc.get(j).col);
-						temp_min = Math.min(temp_min, temp_dist);
-					}
+				else if(temp == 2) { //치킨집일 경우
+					chicken.add(new Pos(i,j));
 				}
-				dist += temp_min;
 			}
-			
-			min = Math.min(min, dist);
-			return;
+		} 
+		
+		min = Integer.MAX_VALUE;
+		visit = new boolean[chicken.size()];
+		func(0,0);
+		System.out.println(min);
+	}
+	static void func(int cnt, int start) {
+		if(cnt ==m) { // m개 골랐으면 각 지집에서 치킨집 거리 구하고 가장 가까운거 distiace 변수에 담아줌
+			calDistance();
 		}
 		
-		for(int i = start; i < kfc.size(); i++) {
-			visit[i] = true;
-			func(cnt+1,i+1);
-			visit[i] = false;
+		for(int i = start; i < chicken.size(); i++) { //조합으로 치킨집 m개 고르고
+			visit[i] = true; 
+			func(cnt+1, i+1);
+			visit[i] = false; //썼으면 반납
 		}
+	}
+	static void calDistance() {
+		int distance = 0;
+		for(int i = 0; i < home.size(); i++) {
+			int temp_min = Integer.MAX_VALUE;
+			for(int j  = 0; j <chicken.size(); j++) {
+				if(visit[j]) {
+					int temp_distance = Math.abs(home.get(i).row - chicken.get(j).row) + Math.abs(home.get(i).col - chicken.get(j).col);
+					temp_min = Math.min(temp_min, temp_distance);
+				}
+			}
+			distance += temp_min;
+		}
+		min = Math.min (min, distance);
+		return;
 	}
 }
