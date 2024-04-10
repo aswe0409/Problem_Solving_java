f72f24852ddbcfe8e5d05d5398a5edfd9bdd4a26
정석영 <aswe0409@naver.com>
2024-04-10 13:41:36 +0900

    [Silver II] Title: N과 M (9), Time: 592 ms, Memory: 37384 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n,m;
+	static int [] num, ret;
+	static boolean [] visit;
+	static StringBuilder sb;
+	static LinkedHashSet<String> ans;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		num = new int[n];
+		ret = new int[m];
+		visit = new boolean[n];
+		ans = new LinkedHashSet<String>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		
+//		for(int i = 0; i < n; i++) {
+//			System.out.println(num[i]);
+//		}
+		func(0);
+		
+		for(String temp : ans) {
+			System.out.println(temp);
+		}
+	}
+	static void func(int cnt) {
+		if(cnt == m) {
+			sb = new StringBuilder();
+			for(int temp : ret ) {
+				sb.append(temp).append(" ");
+			}
+			//sb.append("\n");
+			ans.add(sb.toString());
+			return;
+		}
+		else {
+			for(int i = 0; i < n; i++) {
+				if(!visit[i]) {
+					visit[i] = true;
+					ret[cnt] = num[i];
+					func(cnt+1);
+					visit[i] = false;
+				}
+			}
+		}
+	}
+}
