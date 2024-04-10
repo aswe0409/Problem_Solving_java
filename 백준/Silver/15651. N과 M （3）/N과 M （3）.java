ee4961439758125a91f82b57ce4853b4021cfd7c
정석영 <aswe0409@naver.com>
2024-04-10 13:01:14 +0900

    [Silver III] Title: N과 M (3), Time: 344 ms, Memory: 112056 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int [] ret;
+	public static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		ret = new int[M];
+		comb(0);
+		System.out.println(sb);
+		
+	}
+	
+	private static void comb(int cnt) {
+		if(cnt == M) {
+			for(int num: ret) {
+				sb.append(num).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		else {
+			for(int i = 1; i <= N; i++) {
+				ret[cnt] = i;
+				comb(cnt+1);
+			}
+		}
+	}
+}
