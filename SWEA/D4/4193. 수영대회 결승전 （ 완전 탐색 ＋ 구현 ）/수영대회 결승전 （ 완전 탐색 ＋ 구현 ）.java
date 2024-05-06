8c4a0c5d1fcdd500841b2de2afe5082782ddf45d
정석영 <aswe0409@naver.com>
2024-05-06 18:31:54 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 184 ms, Memory: 36,704 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -3,25 +3,24 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Solution {
-    static class Pos {
-        int row, col, cnt;
-
-        Pos(int row, int col, int cnt) {
-            this.row = row;
-            this.col = col;
-            this.cnt = cnt;
-        }
-    }
-
+	static class Pos{
+		int row, col, cnt;
+		
+		Pos(int row, int col, int cnt){
+			this.row = row;
+			this.col = col;
+			this.cnt = cnt;
+		}
+	}
     static int[] di = { 1, -1, 0, 0 };
     static int[] dj = { 0, 0, -1, 1 };
     static int[][] arr;
     static int n;
     static Queue<Pos> q;
     static boolean[][] visit;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
         int t = sc.nextInt();
         for (int test = 1; test <= t; test++) {
@@ -54,42 +53,42 @@ public class Solution {
             System.out.println("#" + test + " " + ret);
         }
     }
-
-    static int bfs(int nowi, int nowj) {
-        q.offer(new Pos(nowi, nowj, 0));
-        visit[nowi][nowj] = true;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-
-            for (int i = 0; i < size; i++) {
-                Pos temp = q.poll();
-
-                for (int d = 0; d < 4; d++) {
-                    int newi = temp.row + di[d];
-                    int newj = temp.col + dj[d];
-
-                    if (newi >= 0 && newi < n && newj >= 0 && newj < n  &&!visit[newi][newj] && arr[newi][newj] != 1) {
-                        if (arr[newi][newj] == 2) {
-                            if (temp.cnt % 3 == 2) {
-                                q.offer(new Pos(newi, newj, temp.cnt + 1));
-                                visit[newi][newj] = true;
-                            }
-                            else {
-                            	q.offer(new Pos(temp.row, temp.col, temp.cnt+1));
-                            }
-                        }
-                        else if(arr[newi][newj] == 0){
-                            q.offer(new Pos(newi, newj, temp.cnt + 1));
+	static int bfs(int nowi, int nowj) {
+		q.offer(new Pos(nowi, nowj, 0));
+		visit[nowi][nowj] = true;
+		
+		while(!q.isEmpty()) {
+			int size = q.size();
+			
+			for(int i = 0; i < size; i++) {
+				Pos cur = q.poll();
+				
+				for(int d =0; d <4; d++) {
+					int newi = cur.row + di[d];
+					int newj = cur.col + dj[d];
+					
+					if (newi >= 0 && newi < n && newj >= 0 && newj < n  &&!visit[newi][newj] && arr[newi][newj] != 1) {
+						if(arr[newi][newj] == 2) { //소용 돌이 일때
+							if(cur.cnt % 3 ==2) { //다음 칸으로 출발
+								q.offer(new Pos (newi, newj, cur.cnt+1));
+								visit[newi][newj] = true;
+							}
+							else { //다음 칸으로 못가면 그자리에 남아 있기
+								q.offer(new Pos(cur.row, cur.col, cur.cnt+1));
+							}
+						}
+						else if(arr[newi][newj] == 0){ //소용 돌이가 아닐 때
+							q.offer(new Pos(newi, newj, cur.cnt + 1));
                             visit[newi][newj] = true;
-                        }
-                    }
-                    if(newi >=0 && newi < n && newj >= 0 && newj <n &&!visit[newi][newj] && arr[newi][newj] == 4) {
-                        return temp.cnt+1;
-                    }
-                }
-            }
-        }
-        return -1;
-    }
+						}
+					}
+					// 다음 칸이 도착 일 때
+					if(newi >=0 && newi < n && newj >= 0 && newj <n &&!visit[newi][newj] && arr[newi][newj] == 4) {
+						return cur.cnt+1;
+					}
+				}
+			}
+		}
+		return -1;
+	}
 }
