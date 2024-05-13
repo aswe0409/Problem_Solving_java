e16217777df3dc80e3d74107b54fa215db932aea
정석영 <aswe0409@naver.com>
2024-05-13 22:40:59 +0900

    [Silver I] Title: 스타트와 링크, Time: 304 ms, Memory: 15356 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,21 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, ans;
 	static int [][] board;
-	static int [] ret;
+
 	static boolean [] visit;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		board = new int[n+1][n+1];
 		ans = Integer.MAX_VALUE;
-		ret = new int[n/2];
 		visit = new boolean[n+1];
 		
 		StringTokenizer st;
@@ -26,28 +23,16 @@ public class Main {
 			}
 		}
 		
-//		for(int i = 0; i <= n; i++) {
-//			for(int j = 0 ; j <= n; j++) {
-//				System.out.print(board[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
 		func(1,0);
 		System.out.println(ans);
 	}
 	static void func(int start, int cnt) {
 		if(cnt == n/2) {
-//			for(int temp : ret) {
-//				System.out.print(temp+" ");
-//			}
-//			System.out.println();
 			check();
 			return;
 		}
-		
 		else {
 			for(int i = start; i <=n; i++) {
-				ret[cnt]=i;
 				visit[i] = true;
 				func(i+1, cnt+1);
 				visit[i] = false;
@@ -55,14 +40,6 @@ public class Main {
 		}
 	}
 	static void check() {
-//		List<Integer> idxb = new ArrayList<Integer>();
-//		for(int i = 1; i <=n; i++) {
-//			if(visit[i] == false) {
-//				idxb.add(i);
-//				//System.out.print(i+" ");
-//			}
-//		}
-		//System.out.println();
 		int teamA = 0;
 		int teamB = 0;
 		for(int i  = 1; i < n; i++) {
@@ -76,7 +53,10 @@ public class Main {
 			}
 		}
 		int temp = Math.abs(teamA - teamB);
-		//System.out.println(temp);
+		if(temp == 0) {
+			System.out.println(0);
+			System.exit(0);
+		}
 		ans = Math.min(temp, ans);
 	}
 }
