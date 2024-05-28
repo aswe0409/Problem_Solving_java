e1b68f8c6dcdaff61103e68a66bda0bce6798f91
정석영 <aswe0409@naver.com>
2024-05-28 10:30:31 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 174 ms, Memory: 29,184 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -8,15 +8,13 @@ public class Solution {
 	static int [][] arr;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int t = Integer.parseInt(br.readLine());
-		
 		for(int test = 1; test <= t; test++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			n = Integer.parseInt(st.nextToken());
 			x = Integer.parseInt(st.nextToken());
 			
-			arr = new int[n*2][n]; // 가로 늘려주는 이유는 가로만 판별 할 거라 세로 값 가로로 바꿀 거임
+			arr = new int[n*2][n];
 			
 			for(int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -30,45 +28,43 @@ public class Solution {
 					arr[i][j] = arr[j][k];
 				}
 			}
-			int cnt = 0;
+			int ans = 0;
 			for(int i = 0; i < n*2; i++) {
-				boolean state = search(i); //경사로 탐색할 인덱스 넘겨 주기
+				boolean state = search(i);
 				if(state == true) {
-					cnt+=1;
+					ans+=1;
 				}
-			}	
-			System.out.println("#"+test+" "+cnt);
+			}
+			System.out.println("#"+test+" "+ans);
 		}
 	}
 	static boolean search(int idx) {
-		boolean[] visit = new boolean[n];
-		for(int i = 0; i < n-1; i++) {
-			//1. 모든 줄이 같은 값인지 확인 할거임 => 만약 같은 값이면 해당 줄은 활주로
+		boolean [] visit = new boolean[n];
+		for(int i = 0; i <n-1; i++) {
+			//1. 모든 줄이 같은지 확인 => 같은 값이면 이미 활주로
 			if(arr[idx][i] == arr[idx][i+1]) continue;
 			
-			//2. 나랑 옆에 값 차이가 2이상이면 return => 경사로 설치할 수가 없어
-			else if(Math.abs(arr[idx][i] - arr[idx][i+1]) >=2) return false;
+			//2 나랑 옆에값 차이가 2 이사이면 경사로 설치 불가
+			else if(Math.abs(arr[idx][i] - arr[idx][i+1]) >= 2) return false;
 			
-			//3. 나랑 옆에 값 차이가 1 이면 내리막 경사로
-			else if(arr[idx][i] - arr[idx][i+1] ==1) { //만약에 1이면 경사로 설치 할 수 있는지 check
-				//내 위치 부터 내 위치 + x 까지 탐색 해서 만약에 길이 x면 경사로 설치 가능
+			//3. 나랑 옆에 값 차이가 1이면 내리막 
+			else if(arr[idx][i] - arr[idx][i+1] == 1) {
 				for(int j = i+1; j <= i+x; j++) {
 					if (j >= n || arr[idx][j] != arr[idx][i + 1] || visit[j]) {
 						return false;
 					}
-                    visit[j] = true;
-                }
+					visit[j] = true;
+				}
 			}
-			//4. 나랑 옆에 값 차이가 -1 이면 오르막 경사로
+			// 4. 나랑 옆에 값 차이가 -1이면 오르막
 			else if(arr[idx][i] - arr[idx][i+1] == -1) {
-				// 내 위치 부터 반대로 x 까지 탐색 해야 함
 				for(int j = i; j > i -x; j--) {
 					if (j <0 || arr[idx][j] != arr[idx][i] || visit[j]) {
 						return false;
 					}
 					visit[j] = true;
 				}
-			}
+			}	
 		}
 		return true;
 	}
