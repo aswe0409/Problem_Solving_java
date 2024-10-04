86b3bff6e1d0fb5d859dfc4bbc8a1748b69d983e
정석영 <aswe0409@naver.com>
2024-10-04 22:16:24 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 264 ms, Memory: 17356 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,34 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
-import java.util.List;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Deque<Integer> dq = new ArrayDeque<>();
-		List<Integer> ret = new ArrayList<>();
-		
-		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		int K = sc.nextInt();
-		
-		for(int i = 1; i <= N; i++) {
-			dq.add(i);
-		}
-		
-		while(dq.size()!=0) {
-			for(int i = 0; i < K-1; i++) {
-				int temp = dq.removeFirst();
-				dq.add(temp);
-			}
-			ret.add(dq.removeFirst());
-		}
-		System.out.print("<"+ret.get(0));
-		for(int i = 1; i < N; i++) {
-			System.out.print(", "+ret.get(i));
-		}
-		System.out.print(">");
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Deque<Integer> dq = new ArrayDeque<>();
+        ArrayList<Integer> ret = new ArrayList<>();
+
+        for(int i = 1; i <= n; i++){
+            dq.addLast(i);
+        }
+
+        while(!dq.isEmpty()){
+            for(int i = 0; i < k-1; i++){
+                dq.addLast(dq.pollFirst());
+            }
+            ret.add(dq.pollFirst());
+        }
+        System.out.print("<" + ret.get(0));
+        for(int i = 1; i < n; i++){
+            System.out.print(", "+ret.get(i));
+        }
+        System.out.print('>');
+    }
 }
