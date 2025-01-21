f5a2c10434d4b4617ccca4173b296061f7caeb51
정석영 <aswe0409@naver.com>
2025-01-21 14:39:48 +0900

    [Silver II] Title: 종이의 개수, Time: 780 ms, Memory: 317252 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,60 +1,60 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n;
-    static int [][] arr;
-    static int minus = 0, zero = 0, one = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        arr = new int[n][n];
-        for(int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        divied(0,0,n);
-        System.out.println(minus);
-        System.out.println(zero);
-        System.out.println(one);
-    }
-    public static void divied(int row, int col, int size){
-        if(check(row,col, size)){ //다 같은수 이면 해당 값 체크
-            int num = arr[row][col];
-            if(num == -1){
-                    minus += 1;
-                }
-            if(num == 0){
-                zero += 1;
-            }
-            if (num == 1){
-                one += 1;
-            }
-            return;
-        }
-        else{//다 다른 수 이면 9분할
-            int newSize = size / 3;
-            for(int i = row; i < row + size; i+=newSize){
-                for(int j = col; j < col + size; j+=newSize){
-                    divied(i,j,newSize);
-                }
-            }
-        }
-    }
-    public static boolean check(int row, int col, int size){
-        for(int i = row; i < row + size; i++){
-            for(int j = col; j < col + size; j++){
-                if(arr[row][col] != arr[i][j]) { // 처음 요소랑 뒤에 나오는 요소가 다르면 다른 숫자가 있는거임
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	static int minus = 0, zero = 0, plus = 0;
+	static int n;
+	static int [][] map;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		
+		map = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j< n; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		divide(0,0, n);
+		System.out.println(minus);
+		System.out.println(zero);
+		System.out.println(plus);
+	}
+	
+	static void divide(int row, int col, int length) {
+		int target = map[row][col];
+		if(check(row, col, length)) {
+			if(target == -1) {
+				minus +=1;
+			}
+			else if(target == 0) {
+				zero +=1;
+			}
+			else {
+				plus +=1;
+			}
+			return;
+		}
+		int newSize = length /3;
+		for(int i = row; i < row + length; i+= newSize) {
+			for(int j = col; j < col + length; j += newSize) {
+				divide(i,j,newSize);
+			}
+		}
+	}
+	
+	static boolean check(int row, int col, int length) {
+		int target = map[row][col];
+		
+		for(int i = row; i < row + length; i++) {
+			for(int j = col; j < col + length; j++) {
+				if(target != map[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
