45fc3e4c59dee3fe394be2899c4f4f3b1d8fdc37
정석영 <aswe0409@naver.com>
2025-01-28 15:30:34 +0900

    [Silver III] Title: N과 M (6), Time: 76 ms, Memory: 12128 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,16 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n;
-	static int m;
-	static int []num;
-	static int [] ret;
-	static StringBuilder sb = new StringBuilder();
-	
+	static int n,m;
+	static int [] ret, num;
+	static boolean [] visited;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,31 +12,32 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
-		num = new int[n];
 		ret = new int[m];
+		num = new int[n];
+		visited = new boolean[n];
 		
-		StringTokenizer str = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
-			num[i] = Integer.parseInt(str.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		Arrays.sort(num);
-		func(0,0);
-		System.out.println(sb.toString());
+		combi(0,0);
 	}
-	
-	private static void func(int cnt, int start) {
+	static void combi(int cnt, int start) {
 		if(cnt == m) {
-			for(int temp : ret) {
-				sb.append(temp+" ");
+			for(int i = 0; i < m; i++) {
+				System.out.print(ret[i]+ " ");
 			}
-			sb.append('\n');
+			System.out.println();
 			return;
 		}
-		else {
-			for(int i = start; i < n; i++) {
+		for(int i = start; i < n; i++) {
+			if(!visited[i]) {
 				ret[cnt] = num[i];
-				func(cnt +1, i +1);
+				visited[i] = true;
+				combi(cnt + 1, i + 1);
+				visited[i] = false;
 			}
 		}
 	}
