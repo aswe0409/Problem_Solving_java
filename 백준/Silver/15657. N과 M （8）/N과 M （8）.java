e35474d9af81b3db22bcade3c82bdd3e1f0dfa06
정석영 <aswe0409@naver.com>
2025-02-04 15:12:18 +0900

    [Silver III] Title: N과 M (8), Time: 116 ms, Memory: 23636 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -1,16 +1,11 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int n;
-	static int m;
-	static int [] num;
-	static int [] ret;
+	static int n,m;
+	static int [] num, ret;
 	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -19,18 +14,18 @@ public class Main {
 		
 		num = new int[n];
 		ret = new int[m];
-		
-		StringTokenizer str = new StringTokenizer(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < n; i++) {
-			num[i] = Integer.parseInt(str.nextToken());
+			num[i] = Integer.parseInt(st.nextToken());
 		}
 		Arrays.sort(num);
-		func(0,0);
-		System.out.println(sb.toString());
+		
+		combi(0,0);
+		System.out.println(sb);
 	}
 	
-	static void func(int cnt, int start) {
-		if(cnt == m) {
+	static void combi(int cnt, int start) {
+		if(cnt == m ) {
 			for(int temp : ret) {
 				sb.append(temp+" ");
 			}
@@ -38,11 +33,9 @@ public class Main {
 			return;
 		}
 		
-		else {
-			for(int i = start; i < n; i++) {
-				ret[cnt] = num[i];
-				func(cnt+1, i);
-			}
+		for(int i = start; i < n; i++) {
+			ret[cnt] = num[i];
+			combi(cnt + 1,i);
 		}
 	}
 }
