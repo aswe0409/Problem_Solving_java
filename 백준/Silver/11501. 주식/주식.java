9ecaafe6b5e7e283f342e0109fb199ded078d4e8
정석영 <aswe0409@naver.com>
2025-05-07 10:27:47 +0900

    [Silver II] Title: 주식, Time: 976 ms, Memory: 322848 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,34 +1,41 @@
+//import java.io.*;
+//import java.util.*;
+//
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        
+//    }
+//}
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int t = Integer.parseInt(br.readLine());
-		
-		for(int test = 0; test < t; test++) {
-			int n = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			int [] arr = new int[n];
-			
-			for(int i = 0; i < n; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			long maxNum = 0;
-			long ret = 0;
-			for(int i = n -1; i >= 0; i--) {
-				int temp = arr[i];
-				if(temp > maxNum) {
-					maxNum = temp;
-				}
-				else if(temp < maxNum) {
-					ret = ret + maxNum - temp;
-				}
-			}
-			System.out.println(ret);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        for(int test = 0; test < T; test++) {
+        	int n = Integer.parseInt(br.readLine());
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int[] arr = new int[n];
+        	
+        	for(int i = 0; i < n; i++) {
+        		arr[i] = Integer.parseInt(st.nextToken());
+        	}
+        	
+        	int maxNum = 0;
+        	long sum = 0;
+        	for(int i = n - 1; i >= 0; i--) {
+        		if(maxNum > arr[i]) {
+        			sum += maxNum - arr[i];
+        		}
+        		else {
+        			maxNum = arr[i];
+        		}
+        	}
+        	System.out.println(sum);
+        }
+    }
 }
