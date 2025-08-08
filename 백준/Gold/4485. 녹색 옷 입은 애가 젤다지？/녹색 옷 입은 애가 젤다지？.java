c5e70e05fd1fb642016fd9423e30256c6a11d31a
aswe0409 <aswe0409@naver.com>
2025-08-08 09:44:52 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 156 ms, Memory: 18136 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,13 +1,7 @@
-import java.io.IOException;
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int [][]arr; //가중치 담아줄 배열
-	static int n;
-	static int [] di = {-1,1,0,0};
-	static int [] dj = {0,0,-1,1};
-	
 	static class Pos implements Comparable<Pos>{
 		int row, col, cnt;
 		Pos(int row, int col, int cnt){
@@ -17,57 +11,63 @@ public class Main {
 		}
 		@Override
 		public int compareTo(Pos o) {
-			return Integer.compare(cnt, o.cnt);
+			return this.cnt - o.cnt;
 		}
 	}
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		Scanner sc = new Scanner(System.in);
-		int t = 1;
-		while(true) {
-			n = sc.nextInt();
-			if(n == 0) {
-				break;
-			}
-			arr = new int[n][n];
-			
-			for(int i = 0; i<n; i++) {
-				for(int j = 0; j < n; j++) {
-					arr[i][j] = sc.nextInt();
-				}
-			}
-			
-			int temp = bfs();
-			System.out.println("Problem "+t+": "+temp);
-			t+=1;
-		}
-		
-	}
-	
-	static int bfs() {
-		PriorityQueue<Pos> pq = new PriorityQueue<>();
-		boolean [][] visit = new boolean[n][n];
-		
-		pq.offer(new Pos(0,0,arr[0][0]));
-		visit[0][0] = true;
-		
-		while(!pq.isEmpty()) {
-			Pos temp = pq.poll();
-			
-			for(int d = 0; d < 4; d++) {
-				int newi = temp.row + di[d];
-				int newj = temp.col + dj[d];
-				
-				if(newi == n-1 && newj == n-1) {
-					return temp.cnt+ arr[newi][newj];
-				}
-				if(newi >= 0 && newi <n && newj >= 0 && newj <n && !visit[newi][newj]) {
-					visit[newi][newj] = true;
-					pq.offer(new Pos(newi, newj, temp.cnt+ arr[newi][newj]));
-				}
-			}
-			
-		}
-		return 0;
-	}
+	static int n;
+	static int map[][];
+	static int[] di = {0,0,-1,1};
+	static int[] dj = {-1,1,0,0};
+	static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int idx = 1;
+        while(true) {
+            n = Integer.parseInt(br.readLine());
+            if(n == 0) {
+            	break;
+            }
+            
+            map = new int[n][n];
+            
+            for(int i = 0; i < n; i++) {
+            	StringTokenizer st = new StringTokenizer(br.readLine());
+            	for(int j = 0; j < n; j++) {
+            		map[i][j] = Integer.parseInt(st.nextToken());
+            	}
+            }
+            
+            int cnt = bfs(0,0);
+            System.out.println("Problem "+ idx+": "+ cnt);
+            idx += 1;
+        }
+    }
+    
+    static int bfs(int nowi, int nowj) {
+    	PriorityQueue<Pos> pq = new PriorityQueue<>();
+        visited = new boolean[n][n];
+        pq.offer(new Pos(nowi, nowj, map[nowi][nowj]));
+        visited[nowi][nowj] = true;
+        int ret = 0;
+        
+        while(!pq.isEmpty()) {
+        	Pos temp = pq.poll();
+        	
+        	if(temp.row == n-1 && temp.col == n-1) {
+        		ret = temp.cnt;
+        	}
+        	
+        	for(int d = 0; d < 4; d++) {
+        		int newi = temp.row + di[d];
+        		int newj = temp.col + dj[d];
+        		
+        		if(newi >= 0 && newi < n && newj >=0 && newj < n && !visited[newi][newj]) {
+        			pq.offer(new Pos(newi, newj, temp.cnt + map[newi][newj]));
+        			visited[newi][newj] = true;
+        		}
+        	}
+        }
+    	return ret;
+    }
 }
