3e83b8772bfe95ff2419d61eac75b9307e848ba7
aswe0409 <aswe0409@naver.com>
2025-09-22 14:49:53 +0900

    [Silver II] Title: 섬의 개수, Time: 116 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,84 +1,70 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int w, h;
+	static int [][] map;
+	static int [] di = {1,-1,0,0,-1,1,1,-1};
+	static int [] dj = {0,0,1,-1,-1,-1,1,1};
+	static boolean [][] visited;
+	
 	static class Pos{
 		int row, col;
-		public Pos(int row, int col) {
+		Pos(int row, int col){
 			this.row = row;
 			this.col = col;
 		}
 	}
-	
-	static int[][] map;
-	static int[] di = {0,-1,0,1,-1,-1,1,1};
-	static int[] dj = {1,0,-1,0,1,-1,1,-1};
-	static int N, M;
-	static Queue<Pos> q; //bfs 담을 배열
-	
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            N = sc.nextInt();
-            M = sc.nextInt();
-            if(N == 0 && M ==0) break;
-            
-            int cnt = 0;
-            map = new int[M][N]; // 지도
-
-            for(int i = 0; i < M; i++) {
-            	for(int j = 0; j <N; j++) {
-            		map[i][j] = sc.nextInt();
-            	}
-            }
-
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j <N ; j++) {
-                	if(map[i][j] == 1) {
-                		cnt += 1;
-                		//bfs(i,j);
-                		dfs(i,j);
-                	}
-                }
-            }
-            System.out.println(cnt);
-        }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+       while(true) {
+    	   StringTokenizer st = new StringTokenizer(br.readLine());
+    	   w = Integer.parseInt(st.nextToken());
+    	   h = Integer.parseInt(st.nextToken());
+    	   
+    	   if(w == 0 && h == 0) {
+    		   return;
+    	   }
+    	   
+    	   map = new int[h][w];
+    	   visited = new boolean[h][w];
+    	   
+    	   for(int i = 0; i < h; i++) {
+    		   StringTokenizer line = new StringTokenizer(br.readLine());
+    		   for(int j = 0; j < w; j++) {
+    			  map[i][j] = Integer.parseInt(line.nextToken()); 
+    		   }
+    	   }
+    	   int cnt = 0;
+    	   
+    	   for(int i = 0; i < h; i++) {
+    		   for(int j = 0; j < w; j++) {
+    			   if(map[i][j] == 1 && !visited[i][j]) {
+    				   bfs(i,j);
+    				   cnt += 1;
+    			   }
+    		   }
+    	   }
+    	   System.out.println(cnt);
+       }
     }
-    
-//    private static void bfs(int i, int j) {
-//        q = new LinkedList<>();
-//		q.offer(new Pos(i,j)); //bfs 탐색 출발 좌표를 일단 큐에 방문 스케줄 잡자
-//		map[i][j] = 0; // 이 정점 방문 스케줄 잡혔음 표시해놓기! 큐에 스케줄 중복해서 들어 갈 수 있음
-//		
-//    	if(q.isEmpty()) return ;
-//    	
-//    	while(!q.isEmpty()){
-//    		Pos temp = q.poll();
-//    		for(int k=0; k<8; k++) {
-//        		int ni = temp.row + di[k];
-//        		int nj = temp.col + dj[k];
-//    			if(ni >=0 && ni <M && nj >=0 && nj <N && map[ni][nj] == 1) {
-//    				q.offer(new Pos(ni, nj));
-//    				map[ni][nj] = 0;
-//    			}
-//    		}
-//    	}
-//    }
-    
-    private static void dfs(int i, int j) {
-    	map[i][j] =0;
+    static void bfs(int nowi, int nowj) {
+    	Queue<Pos> q = new LinkedList<Pos>();
+    	q.offer(new Pos(nowi, nowj));
+    	visited[nowi][nowj] = true;
     	
-    	for(int k = 0; k < 8; k++) {
-    		int ni = i + di[k];
-    		int nj = j + dj[k];
-    		
-    		if(ni >=0 && ni <M && nj >=0 && nj <N && map[ni][nj] == 1) {
-    			dfs(ni,nj);
+    	while(!q.isEmpty()) {
+    		Pos temp = q.poll();
+    		for(int d = 0; d < 8; d++) {
+    			int newi = di[d] + temp.row;
+    			int newj = dj[d] + temp.col;
+    			
+    			if(newi >= 0 && newi < h && newj >= 0 && newj < w && !visited[newi][newj] && map[newi][newj] == 1) {
+    				q.offer(new Pos(newi, newj));
+    				visited[newi][newj] = true;
+    			}
     		}
-    		
     	}
-    	
     }
-    
 }
