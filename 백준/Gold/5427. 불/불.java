a1402a2dde406d94620423c1f49e13d8855f93d6
aswe0409 <aswe0409@naver.com>
2025-09-23 10:50:26 +0900

    [Gold IV] Title: 불, Time: 660 ms, Memory: 114344 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,116 +1,108 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static char [][] map;
+	static boolean [][] visited;
+	static int w, h, state;
+	static int [] di = {0,0,1,-1}; // 동서남북
+	static int [] dj = {1,-1,0,0};
+	static Queue<Pos> q;
+	static Queue<Pos> peopleQ;
 	static class Pos{
-		int row, col, cnt;
-		public Pos(int row, int col, int cnt) {
+		int row, col;
+		Pos(int row, int col){
 			this.row = row;
 			this.col = col;
-			this.cnt = cnt;
 		}
 	}
-	
-	static int w;
-	static int h;
-	static int starti;
-	static int startj;
-	static char[][] map;
-	static int []di = {0,0,1,-1}; // 우 좌 하 상
-	static int []dj = {1,-1,0,0};
-	static Queue<Pos> q;
-	static Queue<Pos> peopelq;
-	static int state; // 0이면 실패, 1이면 탈출
-	static int ret;
-	static boolean[][] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int tc = Integer.parseInt(br.readLine());
-		
-		for(int test = 0; test < tc; test++) {
-            String[] str = br.readLine().split(" ");
-            w = Integer.parseInt(str[0]);
-            h = Integer.parseInt(str[1]);
-			map = new char [h][w];
-			q = new LinkedList<>();
-			peopelq = new LinkedList<>();
-			state = 0;
-			ret = 0;
-			visited = new boolean[h][w];
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int test = Integer.parseInt(br.readLine());
+    	
+    	for(int t = 0; t < test; t++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine());
+    		w = Integer.parseInt(st.nextToken());
+    		h = Integer.parseInt(st.nextToken());
+    		map = new char[h][w];
+    		visited = new boolean[h][w];
+    		q = new LinkedList<>();
+    		peopleQ = new LinkedList();
+    		state = 0; // 0이면 탈출 전, 1이면 탈출
+    		
+    		int starti = 0;
+    		int startj = 0;
+    		
+    		for(int i = 0; i < h; i++) {
+    			String line = br.readLine();
+    			for(int j = 0; j < w; j++) {
+    				map[i][j] = line.charAt(j);
+    				if(map[i][j] == '@') {
+    					starti = i;
+    					startj = j;
+    					peopleQ.offer(new Pos(i,j));
+    					visited[i][j] = true;
+    				}
+    				else if(map[i][j] == '*') {
+    					q.offer(new Pos(i,j));
+    				}
+    			}
+    		}
+    		
+    		int cnt = 0;
+    		
+    		//탈출 못했으면 계속 돌리기, 불 먼저 출발하고 상근이 탈출 시도
+    		while(!peopleQ.isEmpty() && state == 0) {
+    			fire();
+    			escape();
+    			cnt += 1;
+    		}
+    		if(state == 1) {
+    			System.out.println(cnt);
+    		}
+    		else {
+    			System.out.println("IMPOSSIBLE");
+    		}
+    	} 
+    }
+    
+    static void fire() {
+    	int size = q.size();
+    	for(int i = 0; i < size; i++) {
+    		Pos temp = q.poll();
+    		
+    		for(int d = 0; d < 4; d++) {
+    			int newi = di[d] + temp.row;
+    			int newj = dj[d] + temp.col;
+    			
+    			if(newi >= 0 && newi < h && newj >= 0 && newj < w && map[newi][newj] == '.') {
+    				map[newi][newj] = '*';
+    				q.offer(new Pos(newi, newj));
+    			}
+    		}
+    	}
+    }
 
-		    for (int i = 0; i < h; i++) {
-		        String line = br.readLine();
-		        for (int j = 0; j < w; j++) {
-		            map[i][j] = line.charAt(j);
-		            if (map[i][j] == '*') {
-		                q.offer(new Pos(i, j, 0));
-		            } else if (map[i][j] == '@') {
-		                peopelq.offer(new Pos(i, j, 0));
-		                visited[i][j] = true; 
-		            }
-		        }
-		    }
-	        while (!peopelq.isEmpty() && state == 0) {
-			    fire();
-			    escape();
-	        }
-
-		    
-            if(state == 1) {
-            	System.out.println(ret);
-            }
-            else {
-            	System.out.println("IMPOSSIBLE");
-            }
-
-		}
-
-	}
-	
-	private static void fire() {
-	    int size = q.size(); //불의 개수만큼만 처리
-	    for (int i = 0; i < size; i++) {
-	        Pos temp = q.poll();
-	        for (int k = 0; k < 4; k++) {
-	            int newi = temp.row + di[k];
-	            int newj = temp.col + dj[k];
-
-	            if (newi >= 0 && newi < h && newj >= 0 && newj < w && map[newi][newj] == '.') {
-	                map[newi][newj] = '*';
-	                q.offer(new Pos(newi, newj, temp.cnt + 1));
-	            }
-	        }
-	    }
-	}
-	
-	private static void escape() {
-	    int size = peopelq.size(); // 현재 큐에 있는 사람의 위치 개수만큼만 처리
-	    for (int i = 0; i < size; i++) {
-	        if (state == 1) break; // 탈출 하면 stop 그래야 최소값 나옴
-
-	        Pos temp = peopelq.poll();
-	        for (int k = 0; k < 4; k++) {
-	            int newi = temp.row + di[k];
-	            int newj = temp.col + dj[k];
-
-	            // 탈출 했는지 체크
-	            if (newi < 0 || newi >= h || newj < 0 || newj >= w) {
-	                state = 1; // 탈출 성공
-	                ret = temp.cnt + 1;
-	                return;
-	            }
-
-	            
-	            if (newi >= 0 && newi < h && newj >= 0 && newj < w && map[newi][newj] == '.' && !visited[newi][newj]) {
-	                visited[newi][newj] = true;
-	                peopelq.offer(new Pos(newi, newj, temp.cnt + 1));
-	            }
-	        }
-	    }
-	}
+    static void escape() {
+    	int size = peopleQ.size();
+    	for(int i = 0; i < size; i++) {
+    		Pos temp = peopleQ.poll();
+    		
+    		for(int d = 0; d < 4; d++) {
+    			int newi = di[d] + temp.row;
+    			int newj = dj[d] + temp.col;
+    			
+    			//탈출 체크
+    			if(newi < 0 || newi >= h || newj < 0 || newj >= w) {
+    				state = 1;
+    				return;
+    			}
+    			if(newi >= 0 && newi < h && newj >= 0 && newj < w && map[newi][newj] == '.'&& !visited[newi][newj]) {
+    				visited[newi][newj] = true;
+    				peopleQ.offer(new Pos(newi, newj));
+    			}
+    		}
+    	}
+    }
 }
