fb4d1b6811cd8b8f8506e34259fc06f01081c66e
aswe0409 <aswe0409@naver.com>
2025-09-24 18:20:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.98 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,48 +1,49 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    static class Pos{
-    int row, col, cnt;
-    Pos(int row, int col, int cnt){
-        this.row = row;
-        this.col = col;
-        this.cnt = cnt;
-    }
-}
-    static int[] di = {0,0,1,-1};
-    static int[] dj = {1,-1,0,0};
     static boolean [][] visited;
-    static Queue<Pos> q;
+    static int [] di = {0,0,1,-1};
+    static int [] dj = {1,-1,0,0};
+    static int n,m;
     
-    public int solution(int[][] maps) {
-        int answer = 0;
-        visited = new boolean[maps.length][maps[0].length];
-        q = new LinkedList<>();
+    static class Pos{
+        int row, col, cnt;
+            Pos(int row, int col, int cnt){
+            this.row = row;
+            this.col = col;
+            this.cnt = cnt;
+        }
+    }
+    
+    public int solution(int[][] maps) throws IOException{
+        n = maps.length;
+        m = maps[0].length;
+        visited = new boolean[n][m];
         
-        answer = bfs(0,0, maps);
+        int answer = bfs(0,0, maps);
         return answer;
     }
-    static int bfs(int nowi, int nowj, int[][] map){
-        int n = map.length;    
-        int m = map[0].length;
-        
-        q.offer(new Pos(nowi, nowj, 1));
+    
+    static int bfs(int nowi, int nowj, int[][] maps){
+        Queue<Pos> q = new LinkedList<>();
         visited[nowi][nowj] = true;
+        q.offer(new Pos(nowi, nowj, 1));
         
         while(!q.isEmpty()){
             Pos temp = q.poll();
             
             for(int d = 0; d < 4; d++){
-                int newi = temp.row + di[d];
-                int newj = temp.col + dj[d];
+                int newi = di[d] + temp.row;
+                int newj = dj[d] + temp.col;
                 
-                if(newi == n - 1  && newj ==  m - 1){
-                    return temp.cnt+1;
+                if(newi == n -1 && newj == m - 1){
+                    return temp.cnt + 1;
                 }
                 
-                if(newi >= 0 && newi < n && newj >= 0 && newj < m && !visited[newi][newj] && map[newi][newj] == 1){
-                    q.offer(new Pos(newi, newj, temp.cnt + 1));
+                if(newi >= 0 && newi < n && newj >= 0 && newj < m && !visited[newi][newj] && maps[newi][newj] == 1){
                     visited[newi][newj] = true;
+                    q.offer(new Pos(newi, newj, temp.cnt + 1));
                 }
             }
         }
