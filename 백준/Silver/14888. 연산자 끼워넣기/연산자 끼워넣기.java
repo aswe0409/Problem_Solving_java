dba26e3d25be28e5872ae0759e2f3aa180429fa2
aswe0409 <aswe0409@naver.com>
2025-09-26 21:24:24 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 12424 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,48 +1,49 @@
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int[] oper, nums;
 	static int n;
-	static int [] number;
-	static int [] oper = new int[4];
-	static int max = Integer.MIN_VALUE;
-	static int min = Integer.MAX_VALUE;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		number = new int[n];
-	
-		for(int i = 0; i < n; i++) {
-			number[i] = sc.nextInt();
-		}
-		
-		for(int i = 0; i < 4; i++) {
-			oper[i] = sc.nextInt();
-		}
-		
-		func(number[0], 1);
-		System.out.println(max);
-		System.out.println(min);
-	}
-	
-	static void func(int num, int idx) {
-		if(idx == n) {
-			max = Math.max(num, max);
-			min = Math.min(num, min);
-			return;
-		}
-		
-		for(int i = 0; i <4; i++) {
-			if(oper[i] > 0) {
-				oper[i]--; // 해당 연산자 사용 햇으니까 -1
-				switch(i) {
-				case 0: func(num + number[idx], idx+1); break;
-				case 1: func(num - number[idx], idx+1); break;
-				case 2: func(num * number[idx], idx+1); break;
-				case 3: func(num / number[idx], idx+1); break;
-				}
-				oper[i]++; //사용 후 다시 반납
-			}
-		}
-	}
+    static int maxRet = Integer.MIN_VALUE;
+    static int minRet = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        nums = new int[n];
+        oper = new int[4]; //연산자 넣을 곳
+        
+        for(int i = 0; i < n; i++) {
+        	nums[i] = Integer.parseInt(st.nextToken());
+        }
+        StringTokenizer temp = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 4; i++) {
+        	oper[i] = Integer.parseInt(temp.nextToken());
+        }
+        
+        func(nums[0], 1);
+        System.out.println(maxRet);
+        System.out.println(minRet);
+    }
+    
+    static void func(int num, int cnt) {
+    	if(cnt == n) {
+    		maxRet = Math.max(num, maxRet);
+    		minRet = Math.min(num, minRet);
+    		return;
+    	}
+    	
+    	for(int i = 0; i < 4; i++) {
+    		if(oper[i] > 0) {
+        		oper[i] -= 1; //해당 연산자 사용
+    			switch(i) {
+    			case 0: func(num + nums[cnt], cnt +1); break; 
+    			case 1: func(num - nums[cnt], cnt +1); break; 
+    			case 2: func(num * nums[cnt], cnt +1); break; 
+    			case 3: func(num / nums[cnt], cnt +1); break; 
+    			}
+    			oper[i] += 1;
+    		}
+    	}
+    }
 }
