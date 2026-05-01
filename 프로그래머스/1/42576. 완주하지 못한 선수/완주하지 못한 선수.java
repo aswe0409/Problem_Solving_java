9bec37136811c360ad239a22437d51d155bac377
aswe0409 <aswe0409@naver.com>
2026-05-01 14:50:54 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 90.07 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,25 +3,22 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        // 참가자 기준으로 hashmap 만들기
         
-        HashMap <String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>();
         
         for(String temp : participant){
-            map.put(temp, map.getOrDefault(temp, 0) + 1);    
+            map.put(temp, map.getOrDefault(temp, 0) +1);
         }
         
         for(String temp : completion){
-            map.put(temp, map.getOrDefault(temp, 0) - 1);
+            map.put(temp, map.getOrDefault(temp, 0)  -1);
         }
         
-        for(String key : map.keySet()){
-            int value = map.get(key);
-            if(value == 1){
-                answer = key;
+        for(String temp: map.keySet()){
+            if(map.get(temp) == 1){
+                answer = temp;
             }
         }
-        
         return answer;
     }
 }
