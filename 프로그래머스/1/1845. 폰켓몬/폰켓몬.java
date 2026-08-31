a79a493b235e40ca0b6da7e8b18e71e5877e855a
aswe0409 <aswe0409@naver.com>
2026-08-31 21:18:33 +0900

    [level 1] Title: 폰켓몬, Time: 2.74 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> s = new HashSet();
+        
+        for(int i = 0; i < nums.length; i++){
+            s.add(nums[i]);
+        }
+        
+        if(s.size() > nums.length/2){
+            answer = nums.length /2;
+        }
+        else{
+            answer = s.size();
+        }
+        
+        return answer;
+    }
+}
